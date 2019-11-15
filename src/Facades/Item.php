09c2e40111873fe7262e7b52dcc008e234fe23fb
Andrey Helldar <helldar@ai-rus.com>
2019-11-16 01:45:45 +0300

    Preparing for the release of Sitemap 6

--- src/Facades/Item.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Helldar\Core\Xml\Facades;
-
-use Helldar\Core\Xml\Abstracts\Item as ItemAbstract;
-
-class Item extends ItemAbstract
-{
-}
--- src/Interfaces/ItemInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Helldar\Core\Xml\Interfaces;
-
-interface ItemInterface
-{
-    public function get(): array;
-}
